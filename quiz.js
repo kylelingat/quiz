38f81f008b79e45ab8904c5944bfbc1b03c1694c
kylelingat <kylejlingat@gmail.com>
2016-12-07 08:26:11 -1000

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,44 @@
+/* 
+
+
+kyle lingat
+12-7-16
+
+*/
+
+
+var myArray = ['one', 'two', 10, true]
+
+console.log(myArray[2])
+
+
+function checker(boolean) {
+  if (boolean == true) {
+    console.log(myArray[0] + myArray[1] + myArray[2])
+  }
+  else {
+    console.log('You Shall Not Pass!')
+  }
+}
+
+
+checker(myArray[3])
+
+
+var Batman = [
+  man : true.
+  utilityBelt : ['grappling hook', 'smoke thing', 'perfume'],
+  vehicle : 'batmobile'.
+  sidekick : 'robin'
+]
+
+function batmaaan (mancheck) {
+  if (Batman[0] == true) {
+    console.log(utilityBelt[0] + Batman[2] + Batman [3])
+  }
+  else {
+    console.log('No, Im the Bat')
+  }
+}
+
+batmaaan(Batman[0])
